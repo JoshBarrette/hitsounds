41e7bd2a82009119d43fb95396711ab8d69e0f1c
joshbarrette <joshpbarrette@gmail.com>
2023-11-25 07:06:47 -0600

    Eepy...

--- src/app/MyUploads/page.tsx
+++ src/app/MyUploads/page.tsx
@@ -1,14 +1,8 @@
-"use client";
-
-import { useUser } from "@clerk/nextjs";
 import { trpc } from "../_trpc/client";
 import SoundPlayerList from "../_components/SoundPlayerList";
 
 export default function MyUploads() {
-    const user = useUser();
-    var mySounds = trpc.profile.soundsByUploader.useQuery({
-        userID: user.user?.id as string,
-    }).data;
+    var mySounds = trpc.profile.myUploads.useQuery().data;
 
     return (
         <div>
--- src/server/types.ts
+++ src/server/types.ts
@@ -4,5 +4,5 @@ import type { AppRouter } from "./index";
 type RouterInput = inferRouterInputs<AppRouter>;
 type RouterOutput = inferRouterOutputs<AppRouter>;
 
-type getSoundOutputArray = RouterOutput["search"]["getNSounds"];
-type getSoundOutputSingle = RouterOutput["search"]["getNSounds"][0];
+export type getSoundOutputArray = RouterOutput["search"]["getNSounds"];
+export type getSoundOutputSingle = RouterOutput["search"]["getNSounds"][0];
