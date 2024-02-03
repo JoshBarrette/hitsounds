b039b98f56ca02f64df4d7fa934bfe31c733ea40
joshbarrette <joshpbarrette@gmail.com>
2024-02-02 18:03:57 -0600

    Styling

--- src/app/(client)/_components/Footer.tsx
+++ src/app/(client)/_components/Footer.tsx
@@ -12,36 +12,35 @@ export default function Footer() {
     return (
         <div className="bottom-0 my-10 w-full items-center text-center text-white">
             <div className="mb-2 flex">
-                <hr className="mx-auto w-36" />
+                <hr className="mx-auto w-48" />
             </div>
 
             <div className="flex">
                 <div className="mx-auto flex space-x-10 text-lg">
-                    <div className="flex flex-col text-left">
+                    <div className="flex flex-col space-y-2 text-left">
                         <Link href={"/HowToInstall"}>
                             How to Install Sounds
                         </Link>
 
-                        <a href="https://github.com/JoshBarrette/hitsounds">
+                        <Link href="https://github.com/JoshBarrette/hitsounds">
                             GitHub
-                        </a>
+                        </Link>
                     </div>
 
-                    {historyCookie && (
-                        <div className="flex flex-col text-left">
-                            <p className="mb-2">Recently Played Sounds</p>
-                            {history?.map((historyItem, key) => {
-                                return (
-                                    <Link
-                                        href={`/s/${historyItem.id}`}
-                                        key={key}
-                                    >
-                                        {historyItem.title}
-                                    </Link>
-                                );
-                            })}
-                        </div>
-                    )}
+                    <div className="flex flex-col space-y-0 text-left">
+                        <p className="mb-2">Recently Played Sounds</p>
+                        {history ? (
+                            history.map((historyItem, key) => (
+                                <Link href={`/s/${historyItem.id}`} key={key}>
+                                    {historyItem.title}
+                                </Link>
+                            ))
+                        ) : (
+                            <p>
+                                <i>None</i>
+                            </p>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
